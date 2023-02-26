4f57490ef939a0158c510596087c702369040bb2
Minh Vu <minhngoc02hn@gmail.com>
2023-02-26 22:14:51 +0700

    add wtf sth really important

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -28,9 +28,11 @@ const App: React.FC = () => {
   }, [language]);
 
   useEffect(() => {
-    if (lodash.isEmpty(token)) {
-      navigate('/login');
-    }
+    // TODO: check token
+    navigate('/');
+    // if (lodash.isEmpty(token)) {
+    //   navigate('/login');
+    // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
